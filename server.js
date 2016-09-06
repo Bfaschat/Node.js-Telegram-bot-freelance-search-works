5d63117cd9ab26da9b45aa31f04d6d7e6bbc35a3
talgat55 <lightx_design@yahoo.com>
2016-09-06 23:25:02 +0600

    test

--- server.js
+++ server.js
@@ -5,21 +5,10 @@ var api = new telegram({
     token: config.api
 });
 
-api.on('message', function(message) {
-    var chat_id = message.chat.id;
-
-    // It'd be good to check received message type here
-    // And react accordingly
-    // We consider that only text messages can be received here
-
-    api.sendMessage({
-            chat_id: message.chat.id,
-            text: message.text ? message.text : 'This message doesn\'t contain text :('
-        })
-        .then(function(message) {
-            console.log(message);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-});
+api.getMe()
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
